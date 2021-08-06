360e2363b5642d9136b19c1ebd167b5f91f073f9
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2021-08-06 20:29:33 -0300

    First Controller and Razor page test

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "This is a college project.";
+            ViewData["Developer"] = "Gabriel M. Carossi";
 
             return View();
         }
