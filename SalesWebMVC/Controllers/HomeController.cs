a14df0075459f7451c771e7681b3d3bd9a49ae0b
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2021-08-06 21:03:01 -0300

    First Model-Controller-View-Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
